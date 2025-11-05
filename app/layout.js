1ae71ada4f925932651a41128ba2e5eae5c0de86
sajib hossen <sajibhossen560@gmail.com>
2025-11-05 21:04:39 +0600

    create add new page

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>Home |about </nav>
         {children}
       </body>
     </html>
