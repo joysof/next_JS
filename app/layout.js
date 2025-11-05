6323e85555110f02e9bbe895678908c46a4b95ed
sajib hossen <sajibhossen560@gmail.com>
2025-11-05 21:07:00 +0600

    routing a page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>Home |about </nav>
+        <nav>
+          <li><Link href={'/'}>Home</Link></li>
+          <li>home</li>
+        </nav>
         {children}
       </body>
     </html>
