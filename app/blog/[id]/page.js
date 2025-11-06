f85e9adb403b407eafcf77a492fcb7e59a3a7f27
sajib hossen <sajibhossen560@gmail.com>
2025-11-06 08:22:30 +0600

    add images

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,7 +1,11 @@
+
+import Image from 'next/image'
+import img from '@/public/images/img.jpg'
 export default async function  BlogPage({params}) {
     const {id} =await params
     console.log(id)
     return <div>
         <p>the blog id is {id}</p>
+        <Image src={img} alt="this is a img"/>
     </div>
 }
